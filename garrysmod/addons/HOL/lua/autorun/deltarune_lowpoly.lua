3e3a049f85da9c9b5c86ac508912e435a40239c2
Sitoow <66265572+Sitoow@users.noreply.github.com>
2025-08-23 12:55:51 +0200

    added deltarune

---
+++ garrysmod/addons/HOL/lua/autorun/deltarune_lowpoly.lua
@@ -0,0 +1,37 @@
+--Add Playermodel
+player_manager.AddValidModel( "Light World Kris (Low-Poly)", "models/player/deltarune_lp/kris_lp_lightWorld.mdl" )
+player_manager.AddValidHands( "Light World Kris (Low-Poly)", "models/player/deltarune_lp/c_arms_kris_lp_lightWorld.mdl", 0, "0000000" )
+
+player_manager.AddValidModel( "Dark World Kris (Low-Poly)", "models/player/deltarune_lp/kris_lp_darkWorld.mdl" )
+player_manager.AddValidHands( "Dark World Kris (Low-Poly)", "models/player/deltarune_lp/c_arms_kris_lp_darkWorld.mdl", 0, "0000000" )
+
+
+player_manager.AddValidModel( "Light World Susie (Low-Poly)", "models/player/deltarune_lp/susie_lp_lightWorld.mdl" )
+player_manager.AddValidHands( "Light World Susie (Low-Poly)", "models/player/deltarune_lp/c_arms_susie_lp_lightWorld.mdl", 0, "0000000" )
+
+player_manager.AddValidModel( "Dark World Susie (Low-Poly)", "models/player/deltarune_lp/susie_lp_darkWorld.mdl" )
+player_manager.AddValidHands( "Dark World Susie (Low-Poly)", "models/player/deltarune_lp/c_arms_susie_lp_darkWorld.mdl", 0, "0000000" )
+
+
+player_manager.AddValidModel( "Ralsei (Low-Poly)", "models/player/deltarune_lp/ralsei_lp.mdl" )
+player_manager.AddValidHands( "Ralsei (Low-Poly)", "models/player/deltarune_lp/c_arms_ralsei_lp.mdl", 0, "0000000" )
+
+
+player_manager.AddValidModel( "Light World Noelle (Low-Poly)", "models/player/deltarune_lp/noelle_lp_lightWorld.mdl" )
+player_manager.AddValidHands( "Light World Noelle (Low-Poly)", "models/player/deltarune_lp/c_arms_noelle_lp_lightWorld.mdl", 0, "0000000" )
+
+player_manager.AddValidModel( "Dark World Noelle (Low-Poly)", "models/player/deltarune_lp/noelle_lp_darkWorld.mdl" )
+player_manager.AddValidHands( "Dark World Noelle (Low-Poly)", "models/player/deltarune_lp/c_arms_noelle_lp_darkWorld.mdl", 0, "0000000" )
+
+player_manager.AddValidModel( "Lancer (Low-Poly)", "models/player/deltarune_lp/lancer_lp.mdl" )
+player_manager.AddValidHands( "Lancer (Low-Poly)", "models/player/deltarune_lp/c_arms_lancer_lp.mdl", 0, "0000000" )
+
+player_manager.AddValidModel( "Biblically Accurate Lancer (Low-Poly)", "models/player/deltarune_lp/biblically_accurate_lancer_lp.mdl" )
+player_manager.AddValidHands( "Biblically Accurate Lancer (Low-Poly)", "models/player/deltarune_lp/c_arms_lancer_lp.mdl", 0, "0000000" )
+
+player_manager.AddValidModel( "Light World Berdly Low-Poly)", "models/player/deltarune_lp/berdly_lp_lightWorld.mdl" )
+player_manager.AddValidHands( "Light World Berdly Low-Poly)", "models/player/deltarune_lp/c_arms_berdly_lp_lightWorld.mdl", 0, "0000000" )
+
+player_manager.AddValidModel( "Dark World Berdly Low-Poly)", "models/player/deltarune_lp/berdly_lp_darkWorld.mdl" )
+player_manager.AddValidHands( "Dark World Berdly Low-Poly)", "models/player/deltarune_lp/c_arms_berdly_lp_darkWorld.mdl", 0, "0000000" )
+
